d3e2d1b14b2f14d309f88610fe04d57d9d8471da
lr222gw <lr222gw@student.lnu.se>
2014-11-13 01:19:53 +0100

    Data från alla sidorna

--- index.php
+++ index.php
@@ -17,31 +17,50 @@ function doCurlGetRequest($url){
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Vi ställer in handlern på att inte skriva ut datan när den har hämtat den.
 
         $data = curl_exec($ch);
+
         curl_close($ch);//För att den inte behöver vara öppen mer...
     }
 
     return $data;// returnerar html-datan.
 }
 
-function scrapeOnCoursepress($data){//Designar en unik skrapa för coursepress (då den har id/classer som är unika för sidan).
-
+function scrapeOnCoursepress($data, $baseUrl){//Designar en unik skrapa för coursepress (då den har id/classer som är unika för sidan).
+    $arrayWithNodes=[];
     $dom = new DOMDocument(); //skapar min Dom
     if($dom->loadHTML($data)){ // Om if lyckas så är datan som skickas in giltig, då används den datan i samband med vår dom.
 
         $xpath = new DOMXPath($dom);
+        $nodeList = new DOMNodeList();
 
         $nodeList = $xpath->query('//ul[@id = "blogs-list"]/li//a'); // en ganska rätt beskrivning som ger mig
+
+        foreach($nodeList as $node){// Låter mig göra om från list till array...
+            $arrayWithNodes[] = $node;
+        }
+        $nextNode = $xpath->query('//a[@class = "next page-numbers"]');
+
+        $nextHREF ="";
+        foreach($nextNode as $nextNode){ //tar fram url till nästa sida...
+            //Obs, körs bara om nextNode finns...
+            $nextHREF = $nextNode->getAttribute("href");
+            $newData = doCurlGetRequest($baseUrl.$nextHREF);
+
+            $newData = scrapeOnCoursepress($newData,$baseUrl);
+            foreach($newData as $node){
+                $arrayWithNodes[] = $node; // konverterar till array...
+            }
+
+
+            break; // bryter; använder bara foreach som fulhack..
+        }
+
     }
-
-    return $nodeList;
-
+    return $arrayWithNodes;
 }
+$baseUrl = "http://coursepress.lnu.se";
+$url = "http://coursepress.lnu.se/kurser/";//"https://coursepress.lnu.se/kurser/?bpage=5";
+$data = doCurlGetRequest($url); //Noterade att "/"-tecknet på slutet var viktigt...od och synkronisera denna till GitHub.
 
-
-$data = doCurlGetRequest("http://coursepress.lnu.se/kurser/"); //Noterade att "/"-tecknet på slutet var viktigt...od och synkronisera denna till GitHub.
-$scrpedDataNodeList = scrapeOnCoursepress($data);
+$scrpedDataNodeList = scrapeOnCoursepress($data,$baseUrl);
 echo $data;
-foreach($scrpedDataNodeList as $aa){
-    echo $aa->nodeValue . "<br />";
-}
 var_dump($scrpedDataNodeList);
